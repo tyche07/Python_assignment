c214e6b1c819a4c351d426395814bec99272f45d
ANURADHA KUMARI <117726158+tyche07@users.noreply.github.com>
2025-05-08 12:08:40 +0530

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,19 @@
+# Name- ANURADHA KUMARI
+# Reg no.- 23117107002
+# Biomedical and Robotics Engineering
+
+
+def simple_intrest(P, R, T):
+  
+  SI = P*R*T/100
+  return SI
+
+P= 2000
+R= 2
+T= 3
+
+Intrest= simple_intrest(P, R, T)
+
+print("The simple intrest is:",Intrest)
+
+
