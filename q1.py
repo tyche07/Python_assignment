aceef756c6ea3345d18407e3ce7d2dd52f8e9c61
ANURADHA KUMARI <117726158+tyche07@users.noreply.github.com>
2025-05-08 12:08:10 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,19 @@
+# Name- ANURADHA KUMARI
+# Reg no.- 23117107002
+# Biomedical and Robotics Engineering
+
+
+def sum_and_average(num1, num2, num3):
+  
+  total_sum = num1 + num2 + num3
+  average = total_sum/3
+  return total_sum, average
+
+num1 = 10
+num2 = 40
+num3 = 70
+
+sum_of_numbers, average_of_numbers = sum_and_average(num1, num2, num3)
+
+print("The sum is:",sum_of_numbers)
+print("The average is:",average_of_numbers)
