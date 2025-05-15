9d2518b7bd624e07191858dddfce785b4af5ec5f
ravi1626 <rickyravi1609@gmail.com>
2025-05-16 01:46:53 +0530

    Update q17.py

--- q17.py
+++ q17.py
@@ -4,14 +4,14 @@
 
 import numpy as np
 
-
+# Create
 arr = np.array([10, 20, 30])
 print(f"Original array: {arr}")
 
-
+# Append
 arr = np.append(arr, [40, 50])  
 print(f"Array after appending: {arr}")
 
-
+# Remove (delete element at index 1)
 arr = np.delete(arr, 1)  
 print(f"Array after removal: {arr}")
